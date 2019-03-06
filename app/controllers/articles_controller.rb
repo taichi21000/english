36c2c2a462c73562412e6121fb9a580df9bbd2c6
Taichi Nakatani <t.nakatani30000@gmail.com>
2019-03-06 11:58:16 +0900

    added s3 to production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,20 @@ class ArticlesController < ApplicationController
     @article = Article.find_by(id:params[:id])
   end
 
+  def edit
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def update
+    @article = Article.find_by(id: params[:id])
+    if @article.update_attributes(article_params)
+      redirect_to articles_path, success: '保存に成功しました'
+    else
+      flash.now[:danger] = "保存に失敗しました"
+      render 'edit'
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :url, :articlecover, :description)
